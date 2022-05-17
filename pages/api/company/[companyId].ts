c50a7ca05e54f93f9e88b21fa0231a5eaeea0168
yescine <bouchoucha.yassine@gmail.com>
2022-05-17 15:01:59 +0100

    json local db

--- pages/api/company/[companyId].ts
+++ pages/api/company/[companyId].ts
@@ -1,10 +1,11 @@
 import { NextApiRequest, NextApiResponse } from 'next';
-import {data} from './index'
-
+import data from 'db/company.json'
 
 export default (req:NextApiRequest, res:NextApiResponse) => {
   const {companyId} = req.query
 
-  const company = data.find(elem=>elem._id===companyId)
-  res.send({company})
+    const company = data.find(elem=>elem._id==companyId)
+    console.log(companyId,company)
+    res.send(company)
+
 }
--- pages/api/company/index.ts
+++ pages/api/company/index.ts
@@ -1,7 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
 import ch from "chance";
-
+import fs from 'fs'
 const chance = new ch();
 
 export type CompanyProfile = {
@@ -13,7 +13,7 @@ export type CompanyProfile = {
 };
 
 const createCompany = () => ({
-  _id: chance.guid(),
+  _id: chance.fbid(),
   name: chance.company(),
   adress: chance.address(),
   email: chance.email(),
@@ -25,12 +25,14 @@ export const data = Array.from(Array(10).keys()).map((elem): CompanyProfile => c
 export default function handler(req: NextApiRequest, res: NextApiResponse<CompanyProfile[]>) {
   switch (req.method) {
     case "GET":
+      fs.writeFileSync('./data/company.json',JSON.stringify(data))
       res.status(200).json(data);
       break;
     case "POST":
       const {  number } = req.body;
       const newComp = Array.from(Array(number).keys()).map((elem): CompanyProfile => createCompany());
       data.push(...newComp);
+      fs.writeFileSync('./data/company.json',JSON.stringify(data))
 
     default:
       break;
