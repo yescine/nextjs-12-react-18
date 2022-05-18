9ac3fc7929b94bd619c7a654845658892609487a
yescine <bouchoucha.yassine@gmail.com>
2022-05-18 08:35:53 +0100

    mongo db persistance

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,6 +1,16 @@
 import NextAuth from "next-auth";
 import github from "next-auth/providers/github";
+import { MongoDBAdapter } from "@next-auth/mongodb-adapter";
+import clientPromise from "lib/mongodb";
 
 export default NextAuth({
   providers: [github({ clientId: process.env.OAUTH_GITHUB_ID, clientSecret: process.env.OAUTH_GITHUB_SECRET })],
+  adapter: MongoDBAdapter(clientPromise),
+  session:{
+    strategy:"jwt"
+  },
+  jwt:{
+    secret:"random-string"
+  }
+  
 });
